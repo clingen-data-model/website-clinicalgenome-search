21da183c2f70f6739add6a400c1bc90da88c11cb
Scott G <sgoehringer@users.noreply.github.com>
2020-09-30 12:04:40 -0400

    Update button text to be clearer

--- resources/views/_partials/facts/condition-button.blade.php
+++ resources/views/_partials/facts/condition-button.blade.php
@@ -1,3 +1,3 @@
-<a class="btn btn-default btn-sm pl-2 pr-2 pt-1 pb-1 text-10px" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-	<i class="far fa-caret-square-down"></i> Condition Facts
+<a class="btn btn-default btn-sm pl-2 pr-2 pt-1 pb-1" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+	<span class="text-sm"><i class="far fa-caret-square-down"></i> Expand to see condition facts</span>
 </a>
