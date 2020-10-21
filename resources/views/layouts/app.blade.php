a05f019104d26201fa7d2eb54e22af4b63f31e90
Scott G <sgoehringer@users.noreply.github.com>
2020-10-21 18:36:55 -0400

    commnet out livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-  @livewireStyles
+  {{-- @livewireStyles --}}
 
 
 </head>
@@ -414,6 +414,6 @@
       });
 
     </script>
-    @livewireScripts
+    {{-- @livewireScripts --}}
   </body>
   </html>
