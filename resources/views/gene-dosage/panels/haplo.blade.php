6d0f4f8a5d918b21ac953c5eb202ac2221b75c0e
Scott G <sgoehringer@users.noreply.github.com>
2020-10-30 16:08:18 -0400

    text sizing

--- resources/views/gene-dosage/panels/haplo.blade.php
+++ resources/views/gene-dosage/panels/haplo.blade.php
@@ -8,14 +8,14 @@
       <div class="col-sm-3 text-muted text-right bold">HI Score:</div>
       <div class="col-sm-9 border-left-4 bold">{{ $record->haplo_score }}</div>
     </div>
-    <div class="row pb-3"> 
+    <div class="row pb-3">
       <div class="col-sm-3 text-muted text-right bold">HI Evidence Strength:</div>
       <div class="col-sm-9 border-left-4"><span class="bold">{{ $record->haplo_assertion }}</span>
         <a data-toggle="popover" title="DISCLAIMER" data-placement="bottom" data-trigger="hover"> (Disclaimer)</a>
       </div>
     </div>
     @if (!empty($record->loss_pheno_omim))
-    <div class="row pb-3"> 
+    <div class="row pb-3">
       <div class="col-sm-3 text-muted text-right bold">HI Disease:</div>
       <div class="col-sm-9 border-left-4">
         <ul class="list-unstyled">
@@ -29,14 +29,14 @@
     </div>
     @endif
     @if (!empty($record->loss_pmids))
-    <div class="row pb-3"> 
+    <div class="row pb-3">
       <div class="col-sm-3 text-muted text-right bold">HI Evidence:<div></div></div>
       <div class="col-sm-9 border-left-4">
         <ul class="list-unstyled">
           @foreach ($record->loss_pmids as $loss_pmid)
           <li class="mb-3 pb-3 border-bottom-1">
             <a href="https://pubmed.ncbi.nlm.nih.gov/{{ $loss_pmid['pmid'] }}" class="">PUBMED: {{ $loss_pmid['pmid'] }}</a>
-            <div class="small  summariesShow mt-1" id="collapsesummary1">{{ $loss_pmid['desc'] ?? '' }}</div>
+            <div class="summariesShow mt-1" id="collapsesummary1">{{ $loss_pmid['desc'] ?? '' }}</div>
           </li>
           @endforeach
         </ul>
@@ -44,13 +44,13 @@
     </div>
     @endif
     @if (!empty($record->loss_comments))
-    <div class="row pb-3"> 
+    <div class="row pb-3">
       <div class="col-sm-3 text-muted text-right bold">HI Evidence Comments:</div>
       <div class="col-sm-9 border-left-4"><span class="data_pre">{{ $record->loss_comments }}</span></div>
     </div>
     @endif
     @if (!empty($record->cytoband) && strtoupper(substr($record->cytoband, 0, 1)) == 'X')
-    <div class="row pb-3"> 
+    <div class="row pb-3">
       <div class="col-sm-3 text-muted text-right bold">NOTE:<div></div></div>
       <div class="col-sm-9 border-left-4 bg-light p-3">
         <p>The loss-of-function and triplosensitivity ratings for genes on the X chromosome are made in the context of a male genome to account for the effects of hemizygous duplications or nullizygous deletions. In contrast, disruption of some genes on the X chromosome causes male lethality and the ratings of dosage sensitivity instead take into account the phenotype in female individuals. Factors that may affect the severity of phenotypes associated with X-linked disorders include the presence of variable copies of the X chromosome (i.e. 47,XXY or 45,X) and skewed X-inactivation in females.</p>
