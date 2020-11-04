5ecf8350201b898bb47a4e7b168aa25534750649
Phillip Weller <pweller1@geisinger.edu>
2020-11-03 19:40:58 -0500

    Updated text on ftp download page

--- resources/views/gene-dosage/downloads.blade.php
+++ resources/views/gene-dosage/downloads.blade.php
@@ -29,8 +29,16 @@
             <div class="card">
                 <table class="table table-striped table-hover">
                     <tr>
+                        <td><strong>README</strong>
+                            <div class="small">This files are produced by the ClinGen Dosage Sensitivity Curation Working Group, formerly the International Standards for Cytogenomic Arrays (ISCA) Consortium Evidence-Based Review Committee. The goal of this group is to curate regions of the genome (both single genes and particular genomic regions) with respect to their haploinsufficiency and/or triplosensitivity. </div>
+                        </td>
+                        <td></td>
+                        <td colspan="2" style="text-align:center; vertical-align:middle"><a href="ftp://ftp.clinicalgenome.org/README"><button class="btn btn-default"><i class="fas fa-download"></i> README</button><a></td>
+                        <td class="text-10px" nowrap=""></td>
+                    </tr>
+                    <tr>
                         <td><strong>Dosage Sensitivity Curated Gene List</strong>
-                            <div class="small text-muted">Lorem ipsum dolor sit amet, at suas esse iracundia qui, has electram mediocrem forensibus ex, virtute adipiscing quo cu.</div>
+                            <div class="small">The files in this directory contain data for genes that have been through the review process. These files are updated daily. Files are available for genes and regions localized on both GRCh37 and GRCh38.  The tsv files have a header and contain all of the curation information found on the ClinGen Dosage Sensitivity Map web pages, including disease name (when applicable), PMIDs used as evidence, and comments.</div>
                         </td>
                         <td></td>
                         <td style="text-align:center; vertical-align:middle"><a href="ftp://ftp.clinicalgenome.org/ClinGen_gene_curation_list_GRCh37.tsv"><button class="btn btn-default"><i class="fas fa-download"></i> GRCh37 (tsv)</button><a></td>
@@ -39,7 +47,7 @@
                     </tr>
                     <tr>
                         <td><strong>Dosage Sensitivity Curated Region List</strong>
-                            <div class="small text-muted">Lorem ipsum dolor sit amet, at suas esse iracundia qui, has electram mediocrem forensibus ex, virtute adipiscing quo cu.</div>
+                            <div class="small">The files in this directory contain data for regions that have been through the review process. These files are updated daily. Files are available for genes and regions localized on both GRCh37 and GRCh38.  The tsv files have a header and contain all of the curation information found on the ClinGen Dosage Sensitivity Map web pages, including disease name (when applicable), PMIDs used as evidence, and comments.</div>
                         </td>
                         <td></td>
                         <td style="text-align:center; vertical-align:middle"><a href="ftp://ftp.clinicalgenome.org/ClinGen_region_curation_list_GRCh37.tsv"><button class="btn btn-default"><i class="fas fa-download"></i> GRCh37 (tsv)</button></a></td>
@@ -48,7 +56,7 @@
                     </tr>
                     <tr>
                         <td><strong>Dosage Sensitivity Haploinsufficiency Genes</strong>
-                            <div class="small text-muted">Lorem ipsum dolor sit amet, at suas esse iracundia qui, has electram mediocrem forensibus ex, virtute adipiscing quo cu.</div>
+                            <div class="small">BED files are available for gene curation. There are separate files for haploinsufficiency and triplosensitivity as the BED file only has one column for 'score'. Because the score column expects a number rather than text, "Dosage sensitivity unlikely" is represented by the score of 40, and "Gene associated with autosomal recessive phenotype" is represented by the score of 30.</div>
                         </td>
                         <td></td>
                         <td style="text-align:center; vertical-align:middle"><a href="ftp://ftp.clinicalgenome.org/ClinGen_haploinsufficiency_gene_GRCh37.bed"><button class="btn btn-default"><i class="fas fa-download"></i> GRCh37 (bed)</button></a></td>
@@ -57,7 +65,7 @@
                     </tr>
                     <tr>
                         <td><strong>Dosage Sensitivity Triplosensitivity Genes</strong>
-                            <div class="small text-muted">Lorem ipsum dolor sit amet, at suas esse iracundia qui, has electram mediocrem forensibus ex, virtute adipiscing quo cu.</div>
+                            <div class="small">BED files are available for gene curation. There are separate files for haploinsufficiency and triplosensitivity as the BED file only has one column for 'score'. Because the score column expects a number rather than text, "Dosage sensitivity unlikely" is represented by the score of 40, and "Gene associated with autosomal recessive phenotype" is represented by the score of 30.</div>
                         </td>
                         <td></td>
                         <td style="text-align:center; vertical-align:middle"><a href="ftp://ftp.clinicalgenome.org/ClinGen_triplosensitivity_gene_GRCh37.bed"><button class="btn btn-default"><i class="fas fa-download"></i> GRCh37 (bed)</button></a></td>
@@ -67,25 +75,17 @@
 
                     <tr>
                         <td><strong>Dosage Sensitivity Recurrent CNV</strong>
-                            <div class="small text-muted">Lorem ipsum dolor sit amet, at suas esse iracundia qui, has electram mediocrem forensibus ex, virtute adipiscing quo cu.</div>
+                            <div class="small">These files contains recurrent copy number variations (CNV) that have been, or are in the process of being, reviewed by the recurrent CNV ClinGen DSC Subgroup. This file is available in genome build hg19 and hg38 and can be opened in ChAS software (.aed) or in array analysis software that utilizes (.bed) files as well as the UCSC genome browser. The orange bars represent each recurrent region. The black bars represent the segmental duplication clusters/breakpoints.</div>
                         </td>
                         <td></td>
                         <td style="text-align:center"">
-                            <a href="ftp://ftp.clinicalgenome.org/ClinGen%20recurrent%20CNV%20.bed%20file%20V1.1-hg19.bed"><button class="btn btn-default mt-1 mb-3"><i class="fas fa-download"></i> HG19 (bed)</button></a>
+                            <a href="ftp://ftp.clinicalgenome.org/ClinGen%20recurrent%20CNV%20.bed%20file%20V1.1-hg19.bed"><button class="btn btn-default mt-3 mb-3"><i class="fas fa-download"></i> HG19 (bed)</button></a>
                             <a href="ftp://ftp.clinicalgenome.org/ClinGen%20recurrent%20CNV%20.aed%20file%20V1.1-hg19.aed"><button class="btn btn-default mb-1"><i class="fas fa-download"></i> HG19 (aed)</button></a>
                         </td>
                         <td style="text-align:center">
-                            <a href="ftp://ftp.clinicalgenome.org/ClinGen%20recurrent%20CNV%20.bed%20file%20V1.1-hg38.bed"><button class="btn btn-default mt-1 mb-3"><i class="fas fa-download"></i> HG38 (bed)</button></a>
+                            <a href="ftp://ftp.clinicalgenome.org/ClinGen%20recurrent%20CNV%20.bed%20file%20V1.1-hg38.bed"><button class="btn btn-default mt-3 mb-3"><i class="fas fa-download"></i> HG38 (bed)</button></a>
                             <a href="ftp://ftp.clinicalgenome.org/ClinGen%20recurrent%20CNV%20.aed%20file%20V1.1-hg38.aed"><button class="btn btn-default mb-1"><i class="fas fa-download"></i> HG38 (aed)</button></a>
                         </td>
-                        <td class="text-10px" nowrap=""></td>
-                    </tr>
-                    <tr>
-                        <td><strong>README</strong>
-                            <div class="small text-muted">Lorem ipsum dolor sit amet, at suas esse iracundia qui, has electram mediocrem forensibus ex, virtute adipiscing quo cu.</div>
-                        </td>
-                        <td></td>
-                        <td colspan="2" style="text-align:center; vertical-align:middle"><a href="ftp://ftp.clinicalgenome.org/README"><button class="btn btn-default"><i class="fas fa-download"></i> README</button><a></td>
                         <td class="text-10px" nowrap=""></td>
                     </tr>
 
