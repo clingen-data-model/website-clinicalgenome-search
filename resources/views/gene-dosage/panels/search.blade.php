ee295226f27c46643b4b32dac616ee2b96bb4bcf
Phillip Weller <pweller1@geisinger.edu>
2020-12-10 08:33:28 -0500

    added random string to posts so browser back button correctly caches

--- resources/views/gene-dosage/panels/search.blade.php
+++ resources/views/gene-dosage/panels/search.blade.php
@@ -1,5 +1,5 @@
     <div class='ml-5' style='float:left; width:300px;'>
-        <form class="input-group" method="post" action="/kb/gene-dosage/region_search">
+        <form class="input-group" method="post" action="/kb/gene-dosage/region_search?rref=g{{ Str::random(8) }}">
             @csrf
         <div class="input-group-btn input-btn-region-prefix">
                 <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <span class="action-select-text">{{  $type ?? 'GRCh37' }}</span> <span class="caret"></span>
