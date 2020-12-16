dd1902d516751f61b776f79babbf254592126c8b
Scott G <sgoehringer@users.noreply.github.com>
2020-12-16 12:37:04 -0500

    External Data Attribution link next to name

--- resources/views/_partials/facts/gene-panel.blade.php
+++ resources/views/_partials/facts/gene-panel.blade.php
@@ -1,7 +1,7 @@
 <div class="collapse" id="collapseExample">
 	<div class="row">
 			<div class="col-sm-12  mt-0 pt-0 small">
-					<h4 class="border-bottom-1">Gene Facts</h4>
+					<h4 class="border-bottom-1">Gene Facts <span class=" ml-2" style="font-size:11px"><i class="fas fa-question-circle"></i> <a href='https://clinicalgenome.org/tools/clingen-website-faq/attribution/' class="_blank">External Data Attribution</a></span></h4>
 
 					<dl class="dl-horizontal">
 						<dt>HGNC Symbol</dt>
