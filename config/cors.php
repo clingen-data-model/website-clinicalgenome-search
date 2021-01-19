7fbc4d25f0312d3be8015f01d809a10e6a014f71
Scott G <sgoehringer@users.noreply.github.com>
2021-01-19 13:41:58 -0500

    CORs

--- config/cors.php
+++ config/cors.php
@@ -31,8 +31,8 @@ return [
     /*
      * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-    //'allowed_origins' => ['*'],
-    'allowed_origins' => ['clingen.info', '*.clingen.info', 'clingen.us', '*.clingen.us', '*.clinicalgenome.org', 'clinicalgenome.org'],
+    'allowed_origins' => ['*'],
+    //'allowed_origins' => ['clingen.info', '*.clingen.info', 'clingen.us', '*.clingen.us', '*.clinicalgenome.org', 'clinicalgenome.org'],
     
 
     /*
