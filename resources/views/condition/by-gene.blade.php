167b40224629b4ecaf6b905b7fc7a885e3c97690
Phillip Weller <pweller1@geisinger.edu>
2021-01-22 07:22:15 -0500

    wip

--- resources/views/condition/by-gene.blade.php
+++ resources/views/condition/by-gene.blade.php
@@ -22,9 +22,9 @@
 
 	<div class="col-md-7 text-right mt-2 hidden-sm  hidden-xs">
 		  <ul class="list-inline pb-0 mb-0 small">
-            <li class="text-stats line-tight text-center pl-3 pr-3"><span class="countCurations text-18px">{{ $record->nvalid or "XX" }}</span><br />Gene-Disease Validity<br />Classifications</li>
-            <li class="text-stats line-tight text-center pl-3 pr-3"><span class="countGenes text-18px">{{ !empty($record->dosage_curation_map) ? '1' : '0' }}</span><br />Dosage Sensitivity<br />Classifications</li>
-						<li class="text-stats line-tight text-center pl-3 pr-3"><span class="countEps text-18px">{{ $record->naction or "XX" }}</span><br /> Clinical Actionability<br />Assertions</li>
+            <li class="text-stats line-tight text-center pl-3 pr-3"><span class="countCurations text-18px">{{ $record->nvalid ?? '0' }}</span><br />Gene-Disease Validity<br />Classifications</li>
+            <li class="text-stats line-tight text-center pl-3 pr-3"><span class="countGenes text-18px">{{ $record->ndosage ?? 0' }}</span><br />Dosage Sensitivity<br />Classifications</li>
+						<li class="text-stats line-tight text-center pl-3 pr-3"><span class="countEps text-18px">{{ $record->naction ?? '0' }}</span><br /> Clinical Actionability<br />Assertions</li>
 			</ul>
 
 </div>
