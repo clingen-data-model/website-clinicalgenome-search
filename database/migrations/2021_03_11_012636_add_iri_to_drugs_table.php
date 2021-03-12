0b870576ea58d994f2a25762bcb9e2b67655fc07
Phillip Weller <pweller1@geisinger.edu>
2021-03-12 13:30:13 -0500

    Missed migration file

--- database/migrations/2021_03_11_012636_add_iri_to_drugs_table.php
+++ database/migrations/2021_03_11_012636_add_iri_to_drugs_table.php
@@ -14,7 +14,7 @@ class AddIriToDrugsTable extends Migration
     public function up()
     {
         Schema::table('drugs', function (Blueprint $table) {
-           // $table->string('iri')->unique()->after('ident');
+           $table->string('iri')->unique()->after('ident');
         });
     }
 
