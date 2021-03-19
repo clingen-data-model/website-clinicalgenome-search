26b4d58b5bed0470ea8479444bd812f78f566ddf
Scott G <sgoehringer@users.noreply.github.com>
2021-03-19 15:54:35 -0400

    Email draft

--- resources/views/mail/dashboard/change-notification.blade.php
+++ resources/views/mail/dashboard/change-notification.blade.php
@@ -1,19 +1,26 @@
 @extends('mail.layout.default')
 
 @section('heading')
-
-	<h4>Changes have occured on the following genes:</h4>
-	@foreach ($genes as $gene)
-	<p>{{ $gene }}</p>
-	@endforeach
-
+Changes Notification
 @endsection
 
 @section('content')
-	<h4>To view the entire report, click on the link below.</h4>
-	<a href="https://search.clingen.info/reports/view/{{ $report }}">View Report</a>
+
+Hello,<br/>
+You are receiving this email because we detected genes you are following have curations which have changed as of XXXXXXXXX . A full reports can be found by clicking the link below or accessing your dashboard.
+<br />
+<br />
+	<a href="https://search.clingen.info/reports/view/{{ $report }}" class="button">View Report</a>
+	<br />
+	<hr />
+	<strong>Changes have occured on the following genes:</strong><br/>
+	@foreach ($genes as $gene)
+	<p>{{ $gene }}</p>
+	<br>
+	<a href="https://search.clingen.info/reports/view/{{ $report }}">Click here to view the entire report.</a>
 @endsection
 
 @section('boilerplate')
-	content {{$content ?? "it's me!"}}
+	<strong>About ClinGen - Clinical Genome Resource</strong><br/>
+ClinGen is a National Institutes of Health (NIH)-funded resource dedicated to building an authoritative central resource that defines the clinical relevance of genes and variants for use in precision medicine and research.
 @endsection
