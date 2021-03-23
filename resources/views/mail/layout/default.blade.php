6fcaa9f0a04387e4f0b076d5082cf6812c60503f
Scott G <sgoehringer@users.noreply.github.com>
2021-03-23 08:30:25 -0400

    WIP

--- resources/views/mail/layout/default.blade.php
+++ resources/views/mail/layout/default.blade.php
@@ -122,7 +122,7 @@
 						<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
 							<tr>
 								<td class="content-cell" align="center">
-									This information is not intended for direct diagnostic use or medical decision-making without review by a genetics professional. Individuals should not change their health behavior solely on the basis of information contained. If you have questions about the information contained, please see a health care professional.<br /><br />
+									This information is not intended for direct diagnostic use or medical decision-making without review by a genetics professional. Individuals should not change their health behavior solely on the basis of information contained within. If you have questions about the information contained, please see a health care professional.<br /><br />
 									&copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
 								</td>
 							</tr>
