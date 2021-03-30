90f2580f8e38e51ed6f6912978b03dadd7e08dc2
Scott G <sgoehringer@users.noreply.github.com>
2021-03-30 12:39:16 -0400

    correcting the extra row which created left/right space issues

--- resources/views/_partials/facts/gene-panel.blade.php
+++ resources/views/_partials/facts/gene-panel.blade.php
@@ -1,5 +1,5 @@
 <div class="collapse" id="collapseExample">
-	<div class="row">
+	{{-- <div class="row"> --}}
 			<div class="col-sm-12  mt-0 pt-0 small">
 					<h4 class="border-bottom-1">Gene Facts <span class=" ml-2" style="font-size:11px"><i class="fas fa-question-circle"></i> <a href='https://clinicalgenome.org/tools/clingen-website-faq/attribution/' class="_blank">External Data Attribution</a></span></h4>
 
@@ -113,5 +113,5 @@
 						@endif
 					</dl>
 			</div>
-	</div>
+	{{-- </div> --}}
 </div>
