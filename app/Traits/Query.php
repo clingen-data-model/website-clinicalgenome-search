183c74314c7f828423d0d9d192256cf7c9efcf85
P Weller <pweller1@geisinger.edu>
2023-02-21 13:17:45 -0500

    Changes to query trait

--- app/Traits/Query.php
+++ app/Traits/Query.php
@@ -22,90 +22,8 @@ use App\Minute;
 
 trait Query
 {
-    /**
-     * Return a displayable for replication over time out of the SOP7 results
-     *
-     * @param
-     * @return string
-     */
-	/*public static function query($query, $method = '')
-	{
-		try {
-
-			$begin = Carbon::now();			
-			$response = Genegraph::fetch($query);
-			$end = Carbon::now();
-			$record = new Minute([
-				'system' => 'Search',
-				'subsystem' => $method,
-				'method' => 'query',
-				'start' => $begin,
-				'finish' => $end,
-				'status' => 1
-
-			]);
-			$record->save();
-			Log::info("Query Genegraph: From=" . $method . ", start=" . $begin->format('Y-m-d H:i:s.u')
-						. ', end=' . $end->format('Y-m-d H:i:s.u'));
-		} catch (RequestException $exception) {	// guzzle exceptions and error responses from gql
-
-			Log::info("Guzzle Exception from Genegraph: " . Carbon::now()->format('Y-m-d H:i:s.u'));
-
-			$response = $exception->getResponse();
-			if (is_null($response))				// likely a connection error
-			{
-				$errors = $exception->getHandlerContext();
-				GeneLib::putError($errors['error']);
-				return null;
-			}
-	
-			$code = $response->getStatusCode();
-			$reason = $response->getReasonPhrase();
-			$errors = json_decode($exception->getResponse()->getBody()->getContents(), true);
-
-			if (is_array($errors) && isset($errors["errors"]))
-			{
-				// check if there is a message bag
-				$messages = array_column($errors["errors"], 'message');
-				if (is_array($messages))
-					$errors = implode($messages);
-			}
-
-			GeneLib::putError($errors);
-			
-			return null;
-		
-		} catch (GraphQLMissingData $exception) {		// graphql one
-	
-			Log::info("Generic Exception from Genegraph: " . Carbon::now()->format('Y-m-d H:i:s.u'));
-
-			$errors = $exception->getMessage();
-			
-			GeneLib::putError($errors);
-			
-			return null;
-			
-		} catch (Exception $exception) {		// everything else
-	
-			Log::info("Generic Exception from Genegraph: " . Carbon::now()->format('Y-m-d H:i:s.u'));
-
-			$response = $exception->getResponse();
-			$code = $response->getStatusCode();
-			$reason = $response->getReasonPhrase();
-			$errors = json_decode($exception->getResponse()->getBody()->getContents(), true);
-			
-			GeneLib::putError($errors);
-			
-			return null;
-			
-		};
-
-		return $response;
-	}*/
-
-
 	/**
-     * Return a displayable for replication over time out of the SOP7 results
+     * Run query for a graphql call and format either response data or errors
      *
      * @param
      * @return string
@@ -125,7 +43,16 @@ trait Query
 				'query' => $query
 			]);
 
-			/*$end = Carbon::now();
+			if (!$response->successful())
+			{
+				Log::info("Error from Genegraph: " . Carbon::now()->format('Y-m-d H:i:s.u'));
+
+				GeneLib::putError("Failed to retrieve requested data");
+
+				return null;
+			}
+
+			$end = Carbon::now();
 			$record = new Minute([
 				'system' => 'Search',
 				'subsystem' => $method,
@@ -141,6 +68,7 @@ trait Query
 		
 		} catch (RequestException $exception) {	// guzzle exceptions and error responses from gql
 
+			dd("in request exception");
 			Log::info("Guzzle Exception from Genegraph: " . Carbon::now()->format('Y-m-d H:i:s.u'));
 
 			$response = $exception->getResponse();
@@ -166,17 +94,7 @@ trait Query
 			GeneLib::putError($errors);
 			
 			return null;
-		
-		} catch (GraphQLMissingData $exception) {		// graphql one
-	
-			Log::info("Generic Exception from Genegraph: " . Carbon::now()->format('Y-m-d H:i:s.u'));
 
-			$errors = $exception->getMessage();
-			
-			GeneLib::putError($errors);
-			
-			return null;
-			
 		} catch (Exception $exception) {		// everything else
 	dd($exception);
 			Log::info("Generic Exception from Genegraph: " . Carbon::now()->format('Y-m-d H:i:s.u'));
@@ -196,11 +114,8 @@ trait Query
 
 		$data = json_decode($body);
 
-		//dd($data);
-
 		return $data->data;
 
-		//return $response->data;
 	}
 
 }
