64d8ee828401fa5ffa356ad00a455c44caa64a42
P Weller <pweller1@geisinger.edu>
2024-04-07 16:49:07 -0400

    Fixing the GRCh38 search error

--- app/Gene.php
+++ app/Gene.php
@@ -839,8 +839,8 @@ class Gene extends Model
                     $genes = self::where(function ($query) use ($chr, $start, $stop){
                                         $query->where('is_par', 0)
                                              ->where('chr', (int) $chr)
-                                             ->where('start38', '>=', (int) $stop)
-                                             ->where('stop38', '<=', (int) $start);
+                                             ->where('start38', '<=', (int) $stop)
+                                             ->where('stop38', '>=', (int) $start);
                                    })->orWhere(function ($query) use ($chr, $start, $stop){
                                         $query->where('is_par', 1);
                                         if ((int)$chr == 23) 
