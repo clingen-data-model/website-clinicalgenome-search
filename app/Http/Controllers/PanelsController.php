f1bacf5b6998c963f9c9ddd56db7275ab5e0fdc0
Seth Atam <seth.atam@gmail.com>
2025-11-16 17:20:32 -0500

    Allow user to sync with button

--- app/Http/Controllers/PanelsController.php
+++ app/Http/Controllers/PanelsController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Panel;
+use Illuminate\Support\Facades\Artisan;
 
 class PanelsController extends Controller
 {
     //
+    public function index()
+    {
+        $panels = Panel::orderBy('title')->get();
+        return view('panels', compact('panels'));
+    }
+
+    public function syncGpm(Request $request)
+    {
+        // Run the console command: query:kafka gpm-general-events
+        Artisan::call('query:kafka gpm-general-events');
+
+        return redirect()
+            ->route('panels.index')
+            ->with('status', 'GPM sync started successfully.');
+    }
 }
