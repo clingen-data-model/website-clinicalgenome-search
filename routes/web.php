5d2494c56223b5930de59f033fdfa7278fbcf9a4
Seth Atam <seth.atam@gmail.com>
2025-11-16 17:21:02 -0500

    Add routes for panels management

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,9 @@ Route::get('/reports/view/{id}', 'HomeController@view')->name('dashboard-show-re
 Route::get('/members', 'MembersController@index')->name('members.index');
 Route::post('/members/sync', 'MembersController@sync')->name('members.sync');
 
+Route::get('/panels', [PanelsController::class, 'index'])->name('panels.index');
+Route::post('/panels/sync-gpm', [PanelsController::class, 'syncGpm'])->name('panels.syncGpm');
+
 /*
  * Gene display routes
  */
