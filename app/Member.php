a80a07f45ad1a52c710c9f22d9ba9e68678cc204
Seth Atam <seth.atam@gmail.com>
2026-01-22 16:23:42 -0500

    Fix

--- app/Member.php
+++ app/Member.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Traits\Display;
+use Illuminate\Support\Facades\Artisan;
 
 use Uuid;
 
