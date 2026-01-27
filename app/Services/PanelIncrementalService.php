ebeb9e38c619f5faa6e784a2fd486fe86c04bf37
Seth Atam <seth.atam@gmail.com>
2026-01-27 03:31:29 -0500

    Fix members

--- app/Services/PanelIncrementalService.php
+++ app/Services/PanelIncrementalService.php
@@ -34,14 +34,13 @@ class PanelIncrementalService
         // Full snapshot import – delegate to original importer
         if ($eventType === 'group_checkpoint_event' || $eventType === 'vcep_definition_approval' || $eventType === 'ep_definition_approved') {
             if ($eventType !== 'group_checkpoint_event') {
-                $data = data_get($data, 'data.group');
+                $groupData = data_get($data, 'data.group');
                 $members = data_get($data, 'data.members');
-                dd($members);
             } else {
-                $data = data_get($data, 'data.data');
+                $groupData = data_get($data, 'data.data');
                 $members = data_get($data, 'data.data.members');
             }
-            return $this->panelImportService->create($data, $members);
+            return $this->panelImportService->create($groupData, $members);
         }
 
         $panel = $this->resolvePanelFromKafka($data);
