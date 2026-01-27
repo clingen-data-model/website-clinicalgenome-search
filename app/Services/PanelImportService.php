58e097eb913e444b6ee7a07c34cee4053eb7681a
Seth Atam <seth.atam@gmail.com>
2026-01-27 02:51:54 -0500

    Fix data source

--- app/Services/PanelImportService.php
+++ app/Services/PanelImportService.php
@@ -14,6 +14,7 @@ class PanelImportService
         $panel = null;
 
         if ($expertPanel = data_get($data, 'expert_panel')) {
+            $panel = $this->findOrCreatePanel($data);
             if (data_get($expertPanel, 'affiliation_id')) {
                 $this->createActivities($panel, $data);
             }
